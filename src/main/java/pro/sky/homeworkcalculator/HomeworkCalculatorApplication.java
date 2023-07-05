cabb8da07db6f2eb13d1a5f44ec6bb77c6c5905a
ValeriyKryatov <Kwaler@mail.ru>
2023-07-05 23:08:47 +0300

    Homework Introduction to Maven and Spring Boot. First commit

---
+++ src/main/java/pro/sky/homeworkcalculator/HomeworkCalculatorApplication.java
@@ -0,0 +1,12 @@
+package pro.sky.homeworkcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCalculatorApplication.class, args);
+	}
+}
